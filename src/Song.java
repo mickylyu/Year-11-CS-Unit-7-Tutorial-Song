14628b6a5d8d67641035dc766cdfc145b91d7fc6
mickylyu <mickyl2025@student.cis.edu.hk>
2023-02-25 14:18:55 +0800

    ijdisf

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,41 @@
+
+import java.util.ArrayList;
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+
+    private ArrayList<String>Listener = new ArrayList();
+
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(ArrayList<String>Name){
+        int newListener = 0;
+        for(String i: Name){
+            if (Listener.contains(i.toLowerCase())){
+                continue;
+            }
+            else{
+                Listener.add(i.toLowerCase());
+                newListener += 1;
+            }
+        }
+        return newListener;
+    }
+
+
+
+
+
 }
